da8de792228e168ebc1ee416bfb3f21c756bacae
Martin Lazarov <mlaz@softserveinc.com>
2019-07-10 16:33:36 +0300

    feat: receive PUT request

--- quiz/quizController.js
+++ quiz/quizController.js
@@ -1,9 +1,13 @@
 'use strict'
+const fs = require('fs');
 
 module.exports = function quizController(quizRepository) {
     function question(req, res) {
+        console.log('>>>', req.body, req.params);
+        // fs.writeFile('./file.txt', JSON.str)
         res.json({
-            success: true
+            success: true,
+            message: 'How are you today'
         })
     }
 
--- quiz/quizRoutes.js
+++ quiz/quizRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 module.exports = (Controller) => {
 
-    router.post('/question', Controller.question);
+    router.put('/question', Controller.question);
     return router;
 };
